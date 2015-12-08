99e1c7fe81efe4d436b98147c917e6e4f69c5dba
jonstar <jsskis7@gmail.com>
2015-12-08 14:52:12 -0500

    fix paintboard to erase all 0s

--- js/view.js
+++ js/view.js
@@ -4,12 +4,13 @@ function View(game){
 
 View.prototype.paintBoard = function(){
     for(var i = 0; i < game.boardArray.length; i++){
-      // debugger
       var row = Math.floor(i / 4);
       var col = i % 4;
       var id = "#"+row + col;
       if(game.boardArray[i] !== "0"){
         $(id).text(game.boardArray[i])
+      } else {
+        $(id).text("")
       }
     }
 
