073ea7d63eaa7f3d34fdb6e38c228d1b19fafef1
jonstar <jsskis7@gmail.com>
2015-12-08 15:35:46 -0500

    get colors through 16, fix board logic

--- js/application.js
+++ js/application.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
   game = new Game;
   view = new View(game);
+  view.paintBoard();
+
+  $(document).keydown(function(event){
+    var direction = undefined;
+    switch(event.which){
+      case 37:
+        direction = "left";
+        break
+      case 38:
+        direction = "up";
+        break
+      case 39:
+        direction = "right";
+        break
+      case 40:
+        direction = "down";
+        break;
+    }
+
+    if(direction !== undefined){
+      game.move(direction);
+      game.randomFill();
+      view.paintBoard();
+    }
+  });
 });
